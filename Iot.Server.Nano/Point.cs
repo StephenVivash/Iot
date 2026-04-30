3df9964a48de0cfc3bae19cace6ab80cb7dbd4e6
Stephen Vivash <stephenvivash@hotmail.com>
2026-04-30 14:12:00 +1000

    Nano points

--- Iot.Server.Nano/Point.cs
+++ Iot.Server.Nano/Point.cs
@@ -1,5 +1,17 @@
 namespace Iot.Server.Nano
 {
+
+	public enum PointType
+	{
+		DigitalInput = 1,
+		DigitalOutput = 2,
+		AnalogInput = 3,
+		PwmOutput = 4,
+		Tm1637 = 5,
+		Bmp280 = 6,
+		ShiftInput = 7
+	}
+
 	public sealed class Point
 	{
 		public Point(int id, int deviceId, string name, PointType type, string address)
