781365735f30b860912167e93136f07992ac1c32
Stephen Vivash <stephenvivash@hotmail.com>
2026-09-14 14:04:54 +1000

    Update Nuget

--- Iot.Client.Maui/Services/IotClientLoopService.cs
+++ Iot.Client.Maui/Services/IotClientLoopService.cs
@@ -53,16 +53,16 @@ public sealed class IotClientLoopService : IDisposable
 		int xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx;
 		string server = serverName switch
 		{
-			//"pi51" => "10.13.172.83",
-			//"piz21" => "10.13.172.28",
-			"koala" => "10.13.172.100",
-			"wallaby" => "10.13.172.100",
-			"goanna" => "10.13.172.228",
-			"lora1" => "10.13.172.98",
-			"nano7" => "10.13.172.57",
-			"nano8" => "10.13.172.193",
-			"nano9" => "10.13.172.13",
-			"nano10" => "10.13.172.226",
+			//"pi51" => "10.96.6.83",
+			//"piz21" => "10.96.6.28",
+			"koala" => "10.96.6.100",
+			"wallaby" => "10.96.6.100",
+			"goanna" => "10.96.6.228",
+			"lora1" => "10.96.6.98",
+			"nano7" => "10.96.6.57",
+			"nano8" => "10.96.6.193",
+			"nano9" => "10.96.6.13",
+			"nano10" => "10.96.6.226",
 			_ => serverName + ".local"
 		};
 
